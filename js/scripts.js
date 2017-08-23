a16666322f67f7ca2302c6561f10a3e0b6178a4b
Mitchell Long and LDMacKrell <mitchell@>
2017-08-23 15:37:52 -0700

    store the total score for one round in the playerOne object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,31 @@
 
 // Business Logic
 function Player (totalScore, roundScore){
-  this.totalScore = [] ;
-  this.roundScore = roundScore[];
+  this.totalScore = [totalScore];
+  this.roundScore = [roundScore];
 }
 
-function addArray(){
-var total = playerOne.reduce(function(sum, value) {
-  return sum + value;
-}, 0);
+// function addArray(){
+// var total = playerOne.reduce(function(sum, value) {
+//   return sum + value;
+// }, 0);
+// }
+
+function bust(thisRoll){
+  if(thisRoll > 1){
+    return true;
+  }
+  return false;
+}
+
+
+function combineScore(points){
+  var sum = 0;
+  for (i = 0; i < points.length; i++) {
+    sum += points[i];
+  }
+  console.log(sum);
+  return sum;
 }
 
 
@@ -16,33 +33,34 @@ function roll(){
   var rollResult = Math.floor((Math.random()*6) +1);
   return rollResult;
 }
-function collectRoll(){
-
-}
 
 
-
+// function Player (totalScore, roundScore){
+//   this.totalScore = [] ;
+//   this.roundScore = [];
+// }
 //User Interface Logic
 $(document).ready(function(){
-  // console.log("made it");
+  var playerOne = new Player(0,0);
+  var playerTwo = new Player(0,0);
 
-//create instances of Player (1 &2)
-  var playerOne = [];
-  var playerTwo = [];
-  $("#roll").click(function(event){
-    event.preventDefault();
-
-    // console.log("made it");
+  $("#roll").click(function(){
     var rollScore = roll();
-    playerOne.push(rollScore);
+    var notOne = bust(rollScore);
+    console.log(notOne);
+    playerOne.roundScore.push(rollScore);
     var check = $("#rollScore").append(playerOne);
-
     console.log(playerOne);
 
   });
-  $("#hold").click(function(event){
-    event.preventDefault();
 
+  $("#hold").click(function(){
+    // debugger;
+    var points = playerOne.roundScore;
+    var sumOfRound = combineScore(points);
+    console.log(combineScore(sumOfRound));
+    playerOne.totalScore.push(sumOfRound);
   });
+
 
 });
